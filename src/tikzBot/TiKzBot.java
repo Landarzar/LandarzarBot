c92605b5b6feb779d6fa90b1905dfd6c80c9aadf
Kai Sauerwald <kai.sauerwald@uni-dortmund.de>
2016-09-23 22:03:59 +0200

    Logger options

--- src/tikzBot/TiKzBot.java
+++ src/tikzBot/TiKzBot.java
@@ -6,11 +6,16 @@ package tikzBot;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.ConsoleHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import javax.json.JsonObject;
 
 import net.landarzar.telegram.TelegramBot;
 import net.landarzar.telegram.TelegramBotProperties;
+import net.landarzar.telegram.model.methodes.AnswerInlineQuery;
 import net.landarzar.telegram.model.methodes.GetMe;
 import net.landarzar.telegram.model.methodes.SendMessage;
 import net.landarzar.telegram.model.types.CallbackQuery;
@@ -18,6 +23,9 @@ import net.landarzar.telegram.model.types.ChosenInlineResult;
 import net.landarzar.telegram.model.types.InlineQuery;
 import net.landarzar.telegram.model.types.Message;
 import net.landarzar.telegram.model.types.Update;
+import net.landarzar.telegram.model.types.inline.InlineQueryResult;
+import net.landarzar.telegram.model.types.inline.InlineQueryResultArticle;
+import net.landarzar.telegram.model.types.inline.InputTextMessageContent;
 
 /**
  * @author Kai Sauerwald
@@ -28,6 +36,14 @@ public class TiKzBot extends TelegramBot
 
 	public static void main(String[] args)
 	{
+		System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tF %1$tT  %5$s%6$s%n");
+		Logger log = Logger.getLogger("TelegramBot");
+		log.setLevel(Level.ALL);
+		ConsoleHandler handler = new ConsoleHandler();
+		handler.setFormatter(new SimpleFormatter());
+		handler.setLevel(Level.ALL);
+		log.addHandler(handler);
+
 		TelegramBotProperties tprop = new TelegramBotProperties(275871224, "AAGgkiiX0ddEQMH_RhNR5biZUoXpv8ciEjA");
 
 		// Loading Properties;
@@ -127,8 +143,25 @@ public class TiKzBot extends TelegramBot
 		System.out.println("[InlineQuery] " + query.id + " from " + query.from.first_name);
 
 		if (!query.query.isEmpty()) {
-			SendMessage sm = new SendMessage("" + query.from.id, query.query);
-			this.methodEnqueue(sm);
+			AnswerInlineQuery answer = new AnswerInlineQuery((mr, b) -> {
+				if (!b) {
+					System.out.println("Error Code: " + mr.getErrorCode() + ", Beschreibung: " + mr.getErrorDescrition());
+				}
+			});
+			answer.inline_query_id = query.id;
+
+			InlineQueryResultArticle iqra = new InlineQueryResultArticle();
+			iqra.id = "0";
+			iqra.title = "Test Text";
+			iqra.input_message_content = new InputTextMessageContent("Iam a text");
+			answer.results.add(iqra);
+			iqra = new InlineQueryResultArticle();
+			iqra.id = "1";
+			iqra.title = "Test Text, too";
+			iqra.input_message_content = new InputTextMessageContent("Iam a text, tooo");
+			answer.results.add(iqra);
+
+			this.methodEnqueue(answer);
 		}
 	}
 
